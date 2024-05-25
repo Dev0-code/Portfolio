7cf63a2f97260568e09aa63bf4c829bd2727f495
Amal Nath K <amalnathk03@gmail.com>
2024-05-25 12:21:17 +0530

    Portfolio

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+
+
+//   const backgroundImage = document.getElementById("background-image");
+//   backgroundImage.style.backgroundImage = "url(g:\Visual Studio Code\MEA(R)N\BOOTSTRAP\Portfolio\image\pexels-catiamatos-1072179.jpg)";
+
+//   function setBackgroundImageSize() {
+//     const windowWidth = window.innerWidth;
+//     const windowHeight = window.innerHeight;
+//     backgroundImage.style.width = windowWidth + "px";
+//     backgroundImage.style.height = windowHeight + "px";
+//   }
+
+//   setBackgroundImageSize();
+//   window.addEventListener("resize", setBackgroundImageSize);
